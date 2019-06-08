a2d753c2cc2e7213992f77ec342e8f49e8051c80
Pulkit Kumar <pulkit.kr191@gmail.com>
2019-06-08 14:49:27 +0530

    WIP: - Add base scaffolding

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from stocksense.core.version import get_version
+
+VERSION = get_version()
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='infraless',
+    version=VERSION,
+    description='Run your code at scale without worrying about the cloud',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Pulkit Kumar',
+    author_email='ispulkitkr@gmail.com',
+    url='https://github.com/ispulkit',
+    license='unlicensed',
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_data={'stocksense': ['templates/*']},
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        stocksense = stocksense.main:main
+    """,
+)
