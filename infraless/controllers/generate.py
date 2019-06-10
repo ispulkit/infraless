240ceeb7ca2dc7ddba1ef7ab3a28ea76449fa34d
Pulkit Kumar <pulkit.kr191@gmail.com>
2019-06-10 09:51:14 +0530

    Add generate engine command Add ilconfig validation

--- infraless/controllers/generate.py
+++ infraless/controllers/generate.py
@@ -10,8 +10,8 @@ from cement import Controller, ex
 from PyInquirer import Token, prompt, style_from_dict
 
 from ..controllers.validators import EmptyValidator
+from ..core import config
 from ..core import helpers as hlprs
-from ..core.config import INFRALESS_CONST_CONFIG_FILENAME
 from ..core.exc import InfralessError
 
 style = style_from_dict({
@@ -31,17 +31,7 @@ class Generate(Controller):
         stacked_type = 'nested'
 
     @ex(
-        help='Generate modules and automated code',
-        arguments=[
-            (['-p', '--provider'], {
-                'help': 'the provider to use for data collection.' +
-                'For ex: valueresearchonline.com',
-                'action': 'store',
-                'dest': 'provider'
-            }),
-        ],
-    )
+        help='Generate modules and automated code', )
     def engine(self):
-        if INFRALESS_CONST_CONFIG_FILENAME not in os.listdir(os.curdir):
-            raise InfralessError('No config file found')
+        config.validate_ilconfig()
         hlprs.log("Engine added", "green")
